ecc1e1218df90687f6f50df1c74ae39ce6906fff
dev_intern3 <dev_intern3@17-005-02MM.local>
2018-12-25 15:54:20 +0900

    commit

--- html/pract.php
+++ html/pract.php
@@ -4,6 +4,18 @@
  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
  <title>テスト</title>
 </head>
+<style>
+ body{
+  width:100%;
+  height:auto;
+ }
+ form{
+  width:50%;
+  background-color:rgb(200,200,200);
+  margin:20px;
+  padding:10px;
+ }
+</style>
 <body>
  <form method="POST" action="form2.php">
   <input type="text" name="data">
@@ -12,25 +24,13 @@
  <?php
   $pdo = new PDO ( 'mysql:host=localhost;dbname=php_data;charset=utf8', 'root', '' );
   foreach ( $pdo->query ( 'select * from php_data' ) as $row ) {
-   echo "<form method='POST'  action='reply.php' ><p>$row[id]:$row[post_data]:$row[regist_data]</p><input type='submit' value='返信'><input type='hidden' name='id' value=$row[id]></form>"; 
+   echo "<center><form id='data' method='POST'  action='reply.php' ><p>$row[id]:$row[post_data]:$row[regist_data]</p><input type='submit' value='返信'><input type='hidden' name='id' value=$row[id]></form></center>";
   }
 ?>
 <?php
   $pdo = new PDO ( 'mysql:host=localhost;dbname=php_reply_data;charset=utf8', 'root', '' );
 foreach ( $pdo->query ( 'select * from php_reply_data' ) as $row ) {
-  echo "<p>$row[id]:$row[post_data]:$row[regist_data]</p>";}
- ?>
- <?php
-  try{
-  $pdo = new PDO('mysql:host=localhost;dbname=php_reply_data;charset=utf8','root','');print '続成功';} catch (PDOException $e){
-   print "データベース接続失敗:{.$e->getMessage()}";
-  }
-  $stmt = $pdo->prepare("INSERT INTO php_reply_data (reply_data,regist_data) VALUES (:reply_data,:regist_data)");
-  $reply_data = $_POST["reply_data"];
-  $regist_data = date("Y-m-d H:i:s");
-  $stmt->bindParam(':reply_data',$reply_data,PDO::PARAM_INT);
-  $stmt->bindValue(':regist_data',$regist_data,PDO::PARAM_INT);
-  $stmt->execute();
+  echo "<p>$row[id]:$row[reply_data]:$row[regist_data]</p>";}
  ?>
 </body>
 </html>
--- html/reply.php
+++ html/reply.php
@@ -8,9 +8,8 @@
 <body>
  <?php
   $id = $_POST["id"];
-  echo $id; 
   $pdo = new PDO('mysql:host=localhost;dbname=php_data;charset=utf8','root','');
-  foreach ( $pdo->query ( 'select * from php_data where id={$id}' ) as $row ) {
+  foreach ( $pdo->query ( "select * from php_data where id=".$id ) as $row ) {
      echo "<p>$row[id]:$row[post_data]:$row[regist_data]</p>";}
  ?> 
  <form method="POST" action="pract.php"> 
@@ -22,5 +21,21 @@
   </label>
   <input type="submit" value="返信">
  </form>
+ <?php
+  try{
+   $pdo = new PDO ( 'mysql:host=localhost;dbname=php_reply_data;charset=utf8', 'root', '' ); print '接続成功' ;
+  }
+  catch(PDOException $e){
+   print "データベース接続失敗:{.$e->getMessage()}";
+  }
+  $stmt = $pdo->prepare("INSERT INTO php_reply_data (reply_data,regist_data) VALUES (:reply_data,:regist_data)");
+  $reply_data = $_POST["reply_data"];
+  $regist_data = date("Y-m-d H:i:s");
+  $stmt->bindParam(':reply_data',$reply_data,PDO::PARAM_INT);
+  $stmt->bindValue(':regist_data',$regist_data,PDO::PARAM_INT);
+  $stmt->execute();
+?>
+
 </body>
 </html>
+
