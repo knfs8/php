1757ca7c10afeb53fca3f18f90de6c9ff9212de0
dev_intern3 <dev_intern3@17-005-02MM.local>
2019-01-31 18:09:16 +0900

    last

---
+++ resources/views/top.blade.php
@@ -0,0 +1,11 @@
+<DOCTYPE! html>
+
+<meta charset="UTF-8">
+<html>
+<body>
+	<header>掲示板</header>
+        <form atcion="form.blade.php" method="POST">
+	<input type="button" value="投稿">
+	</form>
+</body>
+</html>
