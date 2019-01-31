372b82b1a88b7f73aed7fee439205a9e3737f2c2
dev_intern3 <dev_intern3@17-005-02MM.local>
2019-01-31 18:02:48 +0900

    data

---
+++ html/project2/database/migrations/2019_01_31_080809_create_articles_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    //データベースで作成する内容
+    public function up()
+    {
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+
+    public function down()
+    {
+        //
+    }
+}
---
+++ project2/resources/views/form.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<meta charset="UTF-8">
+<body>
+	<form method="POST" action="top.blade.php">
+	  <input type="text">
+	  <input type="submit" value="登録">
+	</form>
+
+</body>
+</html>
